4f58377165868f874001ad1361ec76adf9ab269e
makun212 <fightersma@icloud.com>
2026-06-06 23:00:22 +0900

    筋トレカレンダーとタイマーの表示を作りました。

--- muscle_galaxy/muscle_galaxy/settings.py
+++ muscle_galaxy/muscle_galaxy/settings.py
@@ -128,3 +128,6 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'account.User'
+# settings.py の最後
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
